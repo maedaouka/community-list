2ef705c99d6535ee3213aa969fd3bd5769c47921
ouka.maeda <maedaouka@gmail.com>
2020-08-03 23:03:22 +0900

    add name to group table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_27_203231) do
+ActiveRecord::Schema.define(version: 2020_08_03_133556) do
 
   create_table "group_users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2020_07_27_203231) do
     t.string "explanation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
