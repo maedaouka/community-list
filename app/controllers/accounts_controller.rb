004f51c4763d1a01481679e346967da24793f7ca
ouka.maeda <maedaouka@gmail.com>
2020-08-19 14:44:47 +0900

    save twitter data when create account

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,11 +1,27 @@
 class AccountsController < FirebaseController
   # POST /accounts
   def create
-    puts "アカウント　作成"
+    puts "アカウント　作成1"
     super do |decoded_token|
+      puts decoded_token
+      puts "センテンスパラム "
+      puts account_params
       User.create(
-        uid: decoded_token['uid']
-        name: decoded_token['decoded_token'][:payload]['name'],      )
+        uid: decoded_token['uid'],
+        # name: decoded_token['displayName']
+        # screen_name: decoded_token['decoded_token'][:payload]['displayName']
+        twitter_user_id: decoded_token['decoded_token'][:payload]['user_id'],
+        name: decoded_token['decoded_token'][:payload]['name'],
+        image_url: decoded_token['decoded_token'][:payload]['picture'],
+
+        screen_name: account_params["screen_name"],
+
+      )
     end
   end
-end 
+
+  # Only allow a list of trusted parameters through.
+  def account_params
+    params.require(:twitter_user).permit(:screen_name)
+  end
+end
